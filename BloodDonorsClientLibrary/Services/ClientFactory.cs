db6ef2f9ba4a0ab608f6c8305d9ad71a9f64e572
Kamil Mielnik <kamil.mielnik.km@gmail.com>
2017-08-04 01:33:28 +0200

    Started creating personnelclient

---
+++ BloodDonorsClientLibrary/Services/ClientFactory.cs
@@ -0,0 +1,20 @@
+﻿using System.Net.Http;
+
+namespace BloodDonorsClientLibrary.Services
+{
+    public class ClientFactory
+    {
+        private HttpClient client;
+
+        public ClientFactory()
+        {
+            client = new HttpClient();
+        }
+
+        public DonorClient GetDonorClient()
+            => new DonorClient(client);
+
+        public PersonnelClient GetPersonnelClient()
+            => new PersonnelClient(client);
+    }
+}
---
+++ BloodDonorsClientLibrary/Services/PersonnelClient.cs
@@ -0,0 +1,54 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+using BloodDonorsClientLibrary.Commands;
+using BloodDonorsClientLibrary.Extensions;
+using BloodDonorsClientLibrary.Models;
+using Newtonsoft.Json;
+
+namespace BloodDonorsClientLibrary.Services
+{
+    public class PersonnelClient : HttpClientWithAuthorization
+    {
+        public PersonnelClient(HttpClient client) : base(client)
+        {
+        }
+
+        /// <summary>
+        /// Login into database.
+        /// </summary>
+        /// <param name="pesel">Personnel pesel</param>
+        /// <param name="password">Personnel password</param>
+        /// <returns></returns>
+        public override async Task LoginAsync(string pesel, string password)
+        {
+            var loginCredentials = new LoginCredentials
+            {
+                Pesel = pesel,
+                Password = password
+            };
+
+            var loginJson = JsonConvert.SerializeObject(loginCredentials);
+            var response = await Client.PostJsonAsync("personnel/login", new StringContent(loginJson));
+
+            if (response.IsSuccessStatusCode)
+            {
+                var jwtJson = await response.Content.ReadAsStringAsync();
+                var jwt = JsonConvert.DeserializeObject<Jwt>(jwtJson);
+
+                Token = jwt.Token;
+                AddAuthorizationToClient();
+                AutomaticLogout(jwt.Expires);
+            }
+        }
+
+        /// <summary>
+        /// Returns name of logged personnel.
+        /// </summary>
+        /// <returns></returns>
+        public async Task<string> GetNameAsync()
+        {
+            var response = await Client.GetStringAsync("personnel/name");
+            return response;
+        }
+    }
+}
