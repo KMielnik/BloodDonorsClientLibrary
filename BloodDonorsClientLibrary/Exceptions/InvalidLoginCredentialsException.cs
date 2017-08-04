5e110aa53df68641e9813c281daa21b85834536c
Kamil Mielnik <kamil.mielnik.km@gmail.com>
2017-08-04 21:46:53 +0200

    Added new custom exceptions.

---
+++ BloodDonorsClientLibrary/Exceptions/InvalidLoginCredentialsException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace BloodDonorsClientLibrary.Exceptions
+{
+    public class InvalidLoginCredentialsException : Exception
+    {
+        public InvalidLoginCredentialsException()
+        {
+        }
+
+        public InvalidLoginCredentialsException(string message) : base(message)
+        {
+        }
+
+        public InvalidLoginCredentialsException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+    }
+}
