5b15656ac6de31aafd9ac0905ca44d567cb18db7
Kamil Mielnik <kamil.mielnik.km@gmail.com>
2017-08-04 22:32:18 +0200

    New custom exception

---
+++ BloodDonorsClientLibrary/Exceptions/ResouceAlreadyExistsException.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace BloodDonorsClientLibrary.Exceptions
+{
+    public class ResouceAlreadyExistsException : Exception
+    {
+        public ResouceAlreadyExistsException()
+        {
+        }
+
+        public ResouceAlreadyExistsException(string message) : base(message)
+        {
+        }
+
+        public ResouceAlreadyExistsException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected ResouceAlreadyExistsException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
